b9f6a02bbe5ce23af1f122d7f9db0a5a71ba1761
Seif Ammar <57404468+seif1125@users.noreply.github.com>
2021-03-21 19:41:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const menuDom=document.querySelector(".icon");
+const menucontainerDom=document.querySelector(".menu-cont");
+const menulistitemDom = document.querySelectorAll(".menu-list-item");
+let menuShown=false;
+
+menuDom.addEventListener("click",toggleMenu);
+
+function toggleMenu(){
+
+    if(!menuShown){
+        menuDom.className = "icon fas fa-times fa-2x";
+      menucontainerDom.classList.add("open")
+ 
+      menuShown=true;
+    }
+    else{
+        menuDom.className = "icon fas fa-bars fa-2x";
+           menucontainerDom.classList.remove("open");
+
+           menuShown = false;
+    }
+}
