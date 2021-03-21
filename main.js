a04fb1599f490c8ba5f42dc20983fba4f65cb681
seif1125 <57404468+seif1125@users.noreply.github.com>
2021-03-22 01:11:12 +0200

    added portofolio and about sections

--- main.js
+++ main.js
@@ -10,13 +10,15 @@ function toggleMenu(){
     if(!menuShown){
         menuDom.className = "icon fas fa-times fa-2x";
       menucontainerDom.classList.add("open")
+      menulistitemDom.forEach(items=>items.classList.add('open'));
+
  
       menuShown=true;
     }
     else{
         menuDom.className = "icon fas fa-bars fa-2x";
            menucontainerDom.classList.remove("open");
-
+menulistitemDom.forEach((items) => items.classList.remove("open"));
            menuShown = false;
     }
 }
